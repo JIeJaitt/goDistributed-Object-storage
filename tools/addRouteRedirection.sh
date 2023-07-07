3c7ba5c79dc8c1fc8ddb2b97818e8a797fc91578
JIeJaitt <498938874@qq.com>
2023-07-07 16:00:37 +0800

    ✨ feat(脚本): 路由重定义添加和删除脚本

---
+++ tools/addRouteRedirection.sh
@@ -0,0 +1,11 @@
+# 重定向流量回127.0.0.1
+
+sudo route add -host 10.29.2.1 127.0.0.1
+sudo route add -host 10.29.2.2 127.0.0.1
+
+sudo route add -host 10.29.1.1 127.0.0.1
+sudo route add -host 10.29.1.2 127.0.0.1
+sudo route add -host 10.29.1.3 127.0.0.1
+sudo route add -host 10.29.1.4 127.0.0.1
+sudo route add -host 10.29.1.5 127.0.0.1
+sudo route add -host 10.29.1.6 127.0.0.1
